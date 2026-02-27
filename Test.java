b9871d1aeeb28e1c11c76f0221e4c63e87ea8ba9
ajithaaa06 <ajitha.poonati2512@gmail.com>
2026-02-27 23:43:34 +0530

    Intial commit - 27/02/25

--- Test.java
+++ Test.java
@@ -1,17 +1,5 @@
-import java.beans.JavaBean;
-
-public class Test {
-    public Test ( String name, int age){
-        this.name=name;
-        this.age=age;
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Test File");
     }
-    public class Javabasics {
-        public static void main(String[] args) {
-            Test s=new Test("Ram",10) ;
-            System.out.println(s.student());    
-            
-        }
-    }
-
 }
- 
